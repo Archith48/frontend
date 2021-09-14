aea097d6bc949deb17970161c25dd1feadefee21
anusharpatil <80736038+anusharpatil@users.noreply.github.com>
2021-09-14 13:02:03 +0530

    Add files via upload

--- src/Trending.js
+++ src/Trending.js
@@ -1,15 +1,19 @@
-import {makeStyles} from '@material-ui/core/styles';
+import '@fontsource/roboto';
+import { alpha, makeStyles, useTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Divider from '@material-ui/core/Divider';
 import Link from '@material-ui/core/Link'
 import React, { useEffect } from 'react';
+import  {Route as Router} from 'react-router-dom';
 import { useState } from 'react';
-import { Grid } from '@material-ui/core';
-import { Paper } from '@material-ui/core';
-
-import Header1 from './Header1';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import { IconButton } from '@material-ui/core';
+import { useHistory } from "react-router-dom";
 import NavBar from './NavBar';
-
+import Header1 from './Header1';
+import { Grid, Paper } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     grow: {
@@ -25,15 +29,23 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(2),
         margin: 'auto',
         maxWidth: 1500,
+    },
+    page: {
+        background:"white",
+        width:"100%"
     }
 }));
 
-function Trending(props)
+function Trending()
 {
     const [notes,setNotes]=useState([])
     const classes = useStyles();
+    const history = useHistory();
     const preventDefault = (event) => event.preventDefault();
-
+    const handleOnClick = (e)=>{
+        history.push("/question")                
+    }
+   
     useEffect(()=>{
         fetch("http://localhost:3300/trending")
         .then(res=>res.json())
@@ -41,13 +53,14 @@ function Trending(props)
             console.log(data)
         })
     },[])
-
     return(
         <div>
-        <NavBar />
-        <Header1 heading = "Trending Questions"/>
-        <form action="" name = "trending" className ={classes.root}>
-        {notes.map(e =>(
+            <NavBar/>
+            <Header1 heading="Trending questions"/>
+        <Divider/>
+        <Box>
+            <Typography gutterBottom variant="h5" component="h4" color ="#000">
+            {notes.map(e =>(
                 <Paper className={classes.paper}>
                 <Grid container spacing={1}>
                 <Grid alignItems="center" justifyContent="center" item xs={1}>
@@ -72,8 +85,10 @@ function Trending(props)
                 </Grid>
                 </Grid>
                 </Paper>
-            ))}
-        </form>
+            ))}                
+            </Typography>
+            <Divider/>
+        </Box>
         </div>
     )
 }
